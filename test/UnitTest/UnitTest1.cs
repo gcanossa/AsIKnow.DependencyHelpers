f0a7aedd41b6f9744e95a5f41fe33d8315a6c8fd
goldenass <gianmaria.canossa@gmail.com>
2018-01-19 07:44:23 +0100

    utest and pkg update

---
+++ test/UnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using AsIKnow.DependencyHelpers;
+using AsIKnow.DependencyHelpers.EF;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+        //[Fact]
+        //public void Test1()
+        //{
+        //    DependencyCheckerBuilder builder = new DependencyCheckerBuilder(null, null);
+        //    builder.AddEntityFramewrokDbContext<DbContext>("xy", true).WithPostCheckOperation(p=>Task.CompletedTask);
+        //}
+    }
+}
